8202faf364abad2b4a1d1336a47780b4633794de
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-01-27 22:44:55 +0100

    Database opstelling voor vaste pc

---
+++ backend/configHome.py
@@ -0,0 +1,10 @@
+[connector_python]
+socket = /run/mysqld/mysqld.sock
+user = root
+host = 127.0.0.1
+port = 3306
+password = wachtwoord
+database = mydb
+
+[application_config]
+driver = 'SQL Server'
