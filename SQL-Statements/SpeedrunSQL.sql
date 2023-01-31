1b0b3d001c5f5a2250672d7b8c5a32801b4b6acc
LanderDepotter <lander.depotter@student.howest.be>
2023-01-31 11:49:52 +0100

    Documenten toegevoegd
    Alle documenten zijn toegevoegd om te kunnen indienen.

--- SQL-Statements/SpeedrunSQL.sql
+++
@@ -1,3 +0,0 @@
-SELECT idSpel, spelNaam, naam1, TIME(tijd) AS 'score', aantalPalen, moeilijkheidsgraad FROM spel where spelNaam = "Speedrun" and moeilijkheidsgraad = '1' and aantalPalen = '5' 
-order by TIME(tijd)
-asc
